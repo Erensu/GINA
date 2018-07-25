e418d697a1512b81492c2458210acbf7f4012786
GINAProject <balazs.lupsic@gmail.com>
2018-07-25 09:50:28 +0200

    GINA logo is done

--- 30_Evaulation/Matlab_scripts&funcs/plots/main_plot.m
+++ 30_Evaulation/Matlab_scripts&funcs/plots/main_plot.m
@@ -9,21 +9,21 @@ addpath(fullfile(pwd,'parse', 'calculations'))
 t_data = create_data('new_traj.gina');
 i_data = create_data('new_imu.gina');
 
-enu_plot(t_data,'enu','new_enu_plot_enu');
+%enu_plot(t_data,'enu','new_enu_plot_enu');
 enu_plot(t_data,'2D','new_enu_plot_2d');
-enu_plot(t_data,'3D','new_enu_plot_3d');
+%enu_plot(t_data,'3D','new_enu_plot_3d');
 
-trajectroy3d_plot(t_data,'llh','llh','new_trajectroy3d_llh_llh_plot');
-trajectroy3d_plot(t_data,'llh','2D','new_trajectroy3d_llh_2D_plot');
-trajectroy3d_plot(t_data,'llh','3D','new_trajectroy3d_llh_3D_plot');
+%trajectroy3d_plot(t_data,'llh','llh','new_trajectroy3d_llh_llh_plot');
+%trajectroy3d_plot(t_data,'llh','2D','new_trajectroy3d_llh_2D_plot');
+%trajectroy3d_plot(t_data,'llh','3D','new_trajectroy3d_llh_3D_plot');
 
-trajectroy3d_plot(t_data,'ecef','xyz','new_trajectroy3d_ecef_xyz_plot');
-trajectroy3d_plot(t_data,'ecef','3D','new_trajectroy3d_ecef_3D_plot');
+%trajectroy3d_plot(t_data,'ecef','xyz','new_trajectroy3d_ecef_xyz_plot');
+%trajectroy3d_plot(t_data,'ecef','3D','new_trajectroy3d_ecef_3D_plot');
 
-attitude_plot(t_data,'new_attitude_plot');
+%attitude_plot(t_data,'new_attitude_plot');
 google_plot2(t_data,'new_google_plot2');
 
-vector_plot(t_data,i_data,'new_vector_plot')
+%vector_plot(t_data,i_data,'new_vector_plot')
 
 
 
