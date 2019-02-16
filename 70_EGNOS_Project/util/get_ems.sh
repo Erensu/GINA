4bd420f4c40a3f146596ebefe6b2bd717f949048
takacsbence <takacs.bence@epito.bme.hu>
2019-02-16 07:44:27 +0100

    minor updates on get_ems

--- 70_EGNOS_Project/util/get_ems.sh
+++ 70_EGNOS_Project/util/get_ems.sh
@@ -1,52 +1,31 @@
 #!/bin/bash
 #shell script to download hourly ems files for one day and concatenate them
+#the first argument is the year
+#the second one is the day of year
+#the third one is the geostaionary satellite id
+#typical call_ ./get_ems2.sh 2019 46 136 
 
-#the first argument is the number of the days before the current day
-#if no any arguments are given, it downloads the data of the previous day
-if [ "$1" != "" ]; then
-	n=$1
-else
-	n=1
+#check number of arguments
+if [ "$#" != 3 ]; then
+	echo "typical call ./get_ems2.sh 2019 46 136"
+	exit
 fi
 
-#second argument is the geostationary satellite id, 136 by default
-if [ "$2" != "" ]; then
-	prn=$2
-else
-	prn=136
-fi
-
-#get day of year and year from the PC date
-doy=$(date -d "-$n day" +"%j")
-year=$(date -d "-$n day" +"%Y")
-year2=$(date -d "-$n day" +"%y")
-
-#create output directory
-outputdir="../files/ems/"
-if [ ! -e $outputdir ]
-then
-    mkdir $outputdir
-fi
-
-#create output file
-output=$outputdir"y"$year"_d"$doy"_"$prn".ems"
->$output
+year=$1
+printf -v doy "%03d" $2
+prn=$3
 
 #get files from EMS server and concatenate hourly files into a daily one
-for i in {0..9}
+rm h??.ems
+output="y"$year"_d"$doy"_"$prn".ems"
+> $output
+
+for i in {0..23}
 do
-	wget -N -q --show-progress "ftp://131.176.49.48/pub/PRN$prn/y$year/d$doy/h0$i.ems"
-	cat "h0$i.ems" >> $output
-done
-for i in {10..23}
-do
-	wget -N -q --show-progress "ftp://131.176.49.48/pub/PRN$prn/y$year/d$doy/h$i.ems"
-	cat "h$i.ems" >> $output
+	printf -v hour "%02d" $i
+	wget -N "ftp://131.176.49.48/pub/PRN$prn/y$year/d$doy/h$hour.ems"
+	cat "h$hour.ems" >> $output
 done
 rm h??.ems
-
-#show output file properties
-echo "output file details:"
-ls -lh $output
 
 
